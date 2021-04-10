dfc19145a80cbb80baafed8a85d91dce05d3bb78
robertorimini <robertodarimini@gmail.com>
2021-04-10 18:57:38 +0200

    last commit

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet } from "react-native";
 
-const Stack = createStackNavigator ()
+// Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import UsersList from './screens/UsersList';
-import CreateUserSCreen from './screens/CreateUserScreen';
-import UserDetailScreen from './screens/UserDetailScreen';
+// Components
+import CreateUserScreen from "./screens/CreateUserScreen";
+import UserDetailScreen from "./screens/UserDetailScreen";
+import UsersList from "./screens/UsersList";
+
+const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="CreateUsersScreen" component={CreateUserSCreen} />
-      <Stack.Screen name="UsersList" component={UsersList} />
-      <Stack.Screen name="UsersDetailScreen" component={UserDetailScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#621FF7",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="UsersList"
+        component={UsersList}
+        options={{ title: "Users List" }}
+      />
+      <Stack.Screen
+        name="CreateUserScreen"
+        component={CreateUserScreen}
+        options={{ title: "Create a New User" }}
+      />
+      <Stack.Screen
+        name="UserDetailScreen"
+        component={UserDetailScreen}
+        options={{ title: "User Detail" }}
+      />
     </Stack.Navigator>
-  )
+  );
 }
 
 export default function App() {
@@ -31,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
